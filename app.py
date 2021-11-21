89d7d25976ef34152263afe26e4609992e234e24
Yousuf Khan <yousuf.btech2006@gmail.com>
2021-11-21 11:31:30 +0530

    made some changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running After making changes also"
+    return "Flask app is running"
 
 
 if __name__ == "__main__":
